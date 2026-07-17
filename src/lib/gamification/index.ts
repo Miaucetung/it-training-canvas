b2c5559ef93727f1d1ccef0d501066c7e74fc177
Miaucetung <m.mujagic@icloud.com>
2026-07-17 12:47:35 +0200

    chore: remove dead code (superseded CCNAQuestionPoolDialog, unused barrel file, empty placeholder JSONs)

--- src/lib/gamification/index.ts
+++
@@ -1,9 +0,0 @@
-export { gamificationBus } from './events/event-bus';
-export type { GamificationEvent, GamificationEventType, EventPayload } from './events/event-types';
-export type { ProgressState, AchievementRule, StreakState, XpEntry } from './types';
-export { xpToLevel, calculateXpForEvent, applyXpEvent } from './state/xp-engine';
-export { getCurrentStreak, getLongestStreak, updateStreak } from './state/streak-engine';
-export { checkAllAchievements } from './state/achievement-engine';
-export { ACHIEVEMENT_RULES } from './rules/achievement-rules';
-export { XP_VALUES, LEVEL_THRESHOLDS } from './rules/xp-rules';
-export { progressStore, createEmptyState } from './state/progress-store';
